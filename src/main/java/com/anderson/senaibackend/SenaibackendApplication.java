e2a2e93a5201b5b6d25f29a785e54d734eb0041c
Anderson Silva <andersonline62@gmail.com>
2024-06-16 18:48:19 -0300

    initial application

---
+++ src/main/java/com/anderson/senaibackend/SenaibackendApplication.java
@@ -0,0 +1,13 @@
+package com.anderson.senaibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenaibackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenaibackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anderson/senaibackend/SenaibackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anderson.senaibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenaibackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
