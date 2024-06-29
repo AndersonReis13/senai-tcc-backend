c79f01587c2f35522183f49ee64762433f78e7ed
Anderson Silva <andersonline62@gmail.com>
2024-06-29 05:37:41 -0300

    updated client tools

--- src/main/java/com/anderson/senaibackend/dto/ClientDto.java
+++ src/main/java/com/anderson/senaibackend/dto/ClientDto.java
@@ -1,5 +1,6 @@
 package com.anderson.senaibackend.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -8,7 +9,7 @@ public record ClientDto(
                         Long id,
                         @NotBlank String firstName,
                         @NotBlank String lastName,
-                        @NotBlank String email,
+                        @NotBlank @Email String email,
                         @NotBlank @Size(min = 11, max = 11, message = "O cpf tem que conter dois digitos") String cpf,
                         @NotBlank String address,
                         @NotBlank @Size(min = 9, max = 11, message = "número de telefone invalido") String phoneNumber,
