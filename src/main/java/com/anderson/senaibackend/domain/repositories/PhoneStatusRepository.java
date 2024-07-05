abd9b4ab5e3211faa145239bf4f796536bd13b2b
Anderson Silva <143495169+AndersonReis13@users.noreply.github.com>
2024-07-05 06:45:54 -0300

    Delete src/main/java/com/anderson/senaibackend/domain/repositories/PhoneStatusRepository.java

--- src/main/java/com/anderson/senaibackend/domain/repositories/PhoneStatusRepository.java
+++
@@ -1,8 +0,0 @@
-package com.anderson.senaibackend.domain.repositories;
-
-import com.anderson.senaibackend.domain.model.PhoneStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface PhoneStatusRepository extends JpaRepository<PhoneStatus, Long> {
-}
