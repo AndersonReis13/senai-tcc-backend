d9a75e0616baaefa2e36a160512e9db4c6c56368
Anderson Silva <andersonline62@gmail.com>
2024-07-08 17:51:08 -0300

    refactored: ProductDto

--- src/main/java/com/anderson/senaibackend/dto/ProductDto.java
+++ src/main/java/com/anderson/senaibackend/dto/ProductDto.java
@@ -1,11 +1,36 @@
 package com.anderson.senaibackend.dto;
 
+import com.anderson.senaibackend.domain.model.Product;
+import com.anderson.senaibackend.domain.model.enums.ProductStatus;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+
 import java.math.BigDecimal;
 
 public record ProductDto(Long id,
-                         String name,
-                         String brand,
-                         BigDecimal price,
-                         Integer quantity,
+                         @NotBlank(message = "não pode ser vazio")
+                         @JsonProperty(value = "name") String name,
+
+                         @NotBlank(message = "não pode ser vazio")
+                         @JsonProperty(value = "brand") String brand,
+
+                         @PositiveOrZero(message = "o valor tem que ser positivo ou 0")
+                         @JsonProperty(value = "price") BigDecimal price,
+
+                         @NotNull(message = "o valor não pode ser vazio")
+                         @JsonProperty(value = "quantity") Integer quantity,
+
+                         @NotBlank(message = "não pode ser vazio")
                          String productStatus) {
+    public Product toEntity(ProductDto dto){
+        return new Product(dto.id(),
+                dto.name(),
+                dto.brand(),
+                dto.price(),
+                dto.quantity(),
+                ProductStatus.valueOf(dto.productStatus().toUpperCase()));
+    }
 }
