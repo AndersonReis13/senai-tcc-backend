6d064a558498e2cc38cb9d2677f47568c3bd6739
Anderson Silva <andersonline62@gmail.com>
2024-07-08 17:30:36 -0300

    refactored: os

--- src/main/java/com/anderson/senaibackend/domain/model/OS.java
+++ src/main/java/com/anderson/senaibackend/domain/model/OS.java
@@ -1,5 +1,6 @@
 package com.anderson.senaibackend.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
@@ -14,18 +15,23 @@ public class OS {
     private Long id;
 
     @Column(name = "emission_date")
+    @JsonProperty(value = "emission_date")
     private LocalDate emissionDate = LocalDate.now();
 
     @OneToOne
     @JoinColumn(name = "client_id")
+    @JsonProperty(value = "client_id")
     private Client clientId;
 
     @Column(name = "description", length = 250, nullable = false)
+    @JsonProperty(value = "description")
     private String description;
 
     @Column(name = "material", length = 250, nullable = false)
+    @JsonProperty(value = "material")
     private String material;
 
     @Column(name = "estimated_time", nullable = false)
+    @JsonProperty(value = "estimated_time")
     private LocalDateTime estimatedTime;
 }
