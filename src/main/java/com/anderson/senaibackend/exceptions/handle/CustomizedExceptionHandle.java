c56507f4a51bf9696cef9286c2caffe39096ec1b
Anderson Silva <andersonline62@gmail.com>
2024-07-08 17:29:16 -0300

    added: Exceptions argument not valid

--- src/main/java/com/anderson/senaibackend/exceptions/handle/CustomizedExceptionHandle.java
+++ src/main/java/com/anderson/senaibackend/exceptions/handle/CustomizedExceptionHandle.java
@@ -5,11 +5,16 @@ import com.anderson.senaibackend.exceptions.BadRequestFoundException;
 import com.anderson.senaibackend.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class CustomizedExceptionHandle {
@@ -36,6 +41,27 @@ public class CustomizedExceptionHandle {
                 request.getDescription(false)
         );
         return new ResponseEntity<>(errorResponseDto, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDto> handleMethodArgumentNotValid(MethodArgumentNotValidException notValid, WebRequest request){
+        Map<String, String> erros = new HashMap<>();
+
+        notValid.getBindingResult().getAllErrors().forEach(err -> {
+            String field = ((FieldError) err).getField();
+            String message = err.getDefaultMessage();
+            erros.put(field, message);
+        });
+
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+            Arrays.toString(erros.entrySet().toArray()),
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST,
+                new Date(),
+                request.getDescription(false)
+        );
+
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
     }
 
 }
