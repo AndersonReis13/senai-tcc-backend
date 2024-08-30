84831ff102b59214195800410310d3982807166a
Anderson Silva <andersonline62@gmail.com>
2024-08-30 09:28:05 -0300

    implemented AuthenrizationService

---
+++ src/main/java/com/anderson/senaibackend/services/AuthenrizationService.java
@@ -0,0 +1,24 @@
+package com.anderson.senaibackend.services;
+
+import com.anderson.senaibackend.domain.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenrizationService implements UserDetailsService {
+
+    public final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public AuthenrizationService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return employeeRepository.findByEmail(email);
+    }
+}
